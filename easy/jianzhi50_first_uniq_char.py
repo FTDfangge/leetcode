dd10bc66d81a8d7ad642f357175a6e885ec92f68
raynor <506017129@qq.com>
2022-03-12 16:44:34 +0800

    剑指OFFER 50题目

--- easy/jianzhi50_first_uniq_char.py
+++ easy/jianzhi50_first_uniq_char.py
@@ -16,12 +16,28 @@ class Solution:
             else:
                 dic[dic.index(c) + 1] += 1
 
-        for index in range(1, dic.__len__(),2):
+        for index in range(1, dic.__len__(), 2):
             if dic[index] == 1:
                 return dic[index - 1]
 
         return ' '
 
 
+class Solution2:
+    def firstUniqChar(self, s: str) -> str:
+        position = dict()
+        n = len(s)
+        for i, ch in enumerate(s):
+            if ch in position:
+                position[ch] = -1
+            else:
+                position[ch] = i
+        first = n
+        for pos in position.values():
+            if pos != -1 and pos < first:
+                first = pos
+        return ' ' if first == n else s[first]
+
+
 if __name__ == '__main__':
-    print(Solution().firstUniqChar('leetcode'))
+    print(Solution2().firstUniqChar('leetcode'))
