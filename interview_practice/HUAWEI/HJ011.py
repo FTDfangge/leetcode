991cf39f1da8d70a325de79fd7d7d0b84d269948
raynor <506017129@qq.com>
2022-09-04 21:06:14 +0800

    HUAWEI interview practice 10 - 13

---
+++ interview_practice/HUAWEI/HJ011.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022-09-04 7:08 p.m.
+# @Author  : qkzhong
+# @FileName: HJ011.py
+# @Software: PyCharm
+
+# 描述
+# 输入一个整数，将这个整数以字符串的形式逆序输出
+# 程序不考虑负数的情况，若数字含有0，则逆序形式也含有0，如输入为100，则输出为001
+#
+#
+# 数据范围： 0 \le n \le 2^{30}-1 \0≤n≤2
+# 30
+#  −1
+# 输入描述：
+# 输入一个int整数
+#
+# 输出描述：
+# 将这个整数以字符串的形式逆序输出
+
+n = int(input())
+if n == 0:
+    print(0)
+outstr = ""
+while n > 0:
+    outstr += str(n % 10)
+    n //= 10
+print(outstr)
