25404fb9c233622432dcdb999ffa5ffc1f286cd9
50601 <506017129@qq.com>
2023-02-08 02:10:28 +0800

    近些天的练习

--- medium/jianzhi38_permutation.py
+++ medium/jianzhi38_permutation.py
@@ -3,18 +3,18 @@ from typing import List
 
 class Solution:
     def permutation(self, s: str) -> List[str]:
-        def my_permutation(start: str, s: str) -> set:
+        def my_permutation(s: str) -> set:
             if s.__len__() == 1:
                 return set(s)
 
             result = set()
             for idx, i in enumerate(s):
-                temp = my_permutation(i, s[:idx] + s[idx + 1:])
+                temp = my_permutation(s[:idx] + s[idx + 1:])
                 for t in temp:
                     result.add(i + t)
             return result
 
-        ans = my_permutation('', s)
+        ans = my_permutation(s)
         return list(ans)
 
 
